f4c1c97f544327172159683775c675b32861b84f
Alan Burke <alanburke311@gmail.com>
2018-02-14 16:21:18 -0600

    Add files via upload

---
+++ crystal.js
@@ -0,0 +1,19 @@
+var computrNumber = Math.floor(Math.random() * 26)
+//this generates a number that will correspond to a letter//
+
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//this can be referenced for index with 
+console.log(letters.indexOf('g'));
+
+
+if (var computerNumber == userGuess)
+{
+var score = +1
+var turnsLeft = -1
+}
+else
+{
+turnsLeft = -1
+}
